4a0ec0324df7759ec77fa496d6d719f278fdb747
Vladislav Sharshukov <vsharshukov@gmail.com>
2026-01-13 00:31:33 +0300

    TODO added

--- eon/keywords.h
+++ eon/keywords.h
@@ -7,5 +7,7 @@
 // NOTE(vlad): This assumes that C11 is enabled.
 #define thread_local _Thread_local
 
+// TODO(vlad): Don't use attributes directly. We should determine what attribute to use
+//             in '<eon/build_info.h>'.
 #define maybe_unused __attribute__((unused))
 #define noreturn __attribute__((noreturn)) _Noreturn
