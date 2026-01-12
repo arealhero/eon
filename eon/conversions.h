a1403c840bf3bdaf5b65d19e96cfdfc2be30cc64
Vladislav Sharshukov <vsharshukov@gmail.com>
2026-01-13 01:31:49 +0300

    Time conversions added

--- eon/conversions.h
+++ eon/conversions.h
@@ -2,9 +2,9 @@
 
 #include <eon/types.h>
 
-#define KiB(count) ((Size)(count) << 10)
-#define MiB(count) ((Size)(count) << 20)
-#define GiB(count) ((Size)(count) << 30)
+#define TO_KIBI(value) ((Size)(value) << 10)
+#define TO_MEBI(count) ((Size)(count) << 20)
+#define TO_GIBI(count) ((Size)(count) << 30)
 
 #define TO_MILLI(value) ((Size)(value) * 1000)
 #define TO_MICRO(value) (TO_MILLI(value) * 1000)
@@ -16,6 +16,17 @@
 
 #define NANO_TO_MICRO(value) ((Size)(value) / 1000)
 #define NANO_TO_MILLI(value) (NANO_TO_MICRO(value) / 1000)
+
+// NOTE(vlad): Memory conversions.
+
+#define KiB(count) TO_KIBI(count)
+#define MiB(count) TO_MEBI(count)
+#define GiB(count) TO_GIBI(count)
+
+// NOTE(vlad): Time conversions.
+
+#define SECONDS_TO_MICROSECONDS(value) TO_MICRO(value)
+#define NANOSECONDS_TO_MICROSECONDS(value) NANO_TO_MICRO(value)
 
 // NOTE(vlad): Compile-time tests.
 
