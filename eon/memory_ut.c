dfce4b8c355f9c9cdcc5d490677b52e68a8eff1e
Vladislav Sharshukov <vsharshukov@gmail.com>
2026-01-13 03:19:56 +0300

    Tests for overlapping regions removed
    I completely removed these tests for `copy_memory` and conditionally
    disabled them for `move_memory` if asan is enabled. See the comment
    in `test_copy_memory` for details.

--- eon/memory_ut.c
+++ eon/memory_ut.c
@@ -1,5 +1,7 @@
 #include <eon/unit_test.h>
 
+#include <eon/sanitizers/asan.h>
+
 #define TEST_STRING_LENGTH 10
 
 internal String
@@ -63,27 +65,32 @@ test_copy_memory(Test_Context* context)
         ASSERT_STRINGS_ARE_EQUAL(destination_string, "01234xxxxx");
     }
 
-    // NOTE(vlad): Destination's suffix is the same as a source's prefix.
-    {
-        String string = create_source_string(context->arena);
-
-        copy_memory(as_bytes(string.data),
-                    as_bytes(string.data + 2),
-                    5);
-
-        ASSERT_STRINGS_ARE_EQUAL(string, "2345656789");
-    }
-
+    // NOTE(vlad): I initially wrote two tests here in which the destination overlaps
+    //             with the source. Turns out the compiler is smart enough to see that
+    //             we are copying overlapping memory and therefore rewriting the source
+    //             data. Despite the 'restrict' keyword in the 'copy_memory' definition
+    //             it would emit a code that takes those overlaps into account, thus
+    //             the behaviour of 'copy_memory' is unintentionally correct (as though
+    //             we'd called 'move_memory') when the test was compiled with '-O2', and
+    //             intentionally incorrect with '-O0'.
+    //
+    //             Futhermore, this triggered the 'memcpy-param-overlap' asan check, which
+    //             is fair enough, albeit somewhat unexpected.
+    //
+    //             We could in theory try to detect the current optimisation level and
+    //             optionally enable these tests, but that seems unreasonable to me.
+    //             Another option that we could try is removing the 'restrict' keywords
+    //             from the definition of 'copy_memory', but I really don't want to do it:
+    //             'memcpy' marks its arguments as restricted, because one must use
+    //             'memmove' for overlapping regions.
+    //
+    //             That said, I removed the tests.
+    //
+    //             Overall, I see this as a win because it showed that
+    //               1. the compiler will detect at least some of these incorrect usages
+    //                  and emit correct instructions to restore the expected behaviour;
+    //               2. asan can detect these erroneous usages and report them.
     // NOTE(vlad): Destination's prefix is the same as a source's suffix.
-    {
-        String string = create_source_string(context->arena);
-
-        copy_memory(as_bytes(string.data + 2),
-                    as_bytes(string.data),
-                    5);
-
-        ASSERT_STRINGS_ARE_EQUAL(string, "0101010789");
-    }
 
     // NOTE(vlad): Source and destination are the same.
     {
@@ -124,6 +131,9 @@ test_move_memory(Test_Context* context)
         ASSERT_STRINGS_ARE_EQUAL(destination_string, "01234xxxxx");
     }
 
+    // NOTE(vlad): Read the comment in 'test_copy_memory' for an explanation of
+    //             why we need to disable these tests when asan is enabled.
+#if ASAN_ENABLED == 0
     // NOTE(vlad): Destination's suffix is the same as a source's prefix.
     {
         String string = create_source_string(context->arena);
@@ -145,6 +155,7 @@ test_move_memory(Test_Context* context)
 
         ASSERT_STRINGS_ARE_EQUAL(string, "0101234789");
     }
+#endif
 
     // NOTE(vlad): Source and destination are the same.
     {
